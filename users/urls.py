06da1d623f5cdd57b182809797d3dcbb8170262e
Falvare <77181743+Falvare@users.noreply.github.com>
2022-06-20 23:48:54 -0600

    Added edit user url

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register, profile
+from .views import register, profile, editProfile
 
 from django.contrib.auth.views import LoginView, LogoutView
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('profile/', profile, name='profile')
+    path('profile/', profile, name='profile'),
+    path('edit-profile/', editProfile, name='edit profile')
 ]
