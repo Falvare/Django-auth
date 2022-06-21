0585bb49ee6d124a9082f192c15a29d342365ebe
Falvare <77181743+Falvare@users.noreply.github.com>
2022-06-21 12:08:31 -0600

    Restricted users from changing username

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .forms import RegistrationForm, UserEditForm
 
